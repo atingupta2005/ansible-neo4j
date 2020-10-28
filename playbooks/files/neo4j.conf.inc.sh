d270acf9c4b007a1b0625a8b441b306022f6f300
Atin Gupta <atingupta2005@gmail.com>
2020-10-28 19:25:20 +0530

    -

---
+++ playbooks/files/neo4j.conf.inc.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function join_by { local d=$1; shift; local f=$1; shift; printf %s "$f" "${@/#/$d}"; }
+ 
+# Declare an array of string with type
+declare -a StringArray=("atingupta-20201028-vm1.eastus.cloudapp.azure.com" "atingupta-20201028-vm2.eastus.cloudapp.azure.com" "atingupta-20201028-vm3.eastus.cloudapp.azure.com" "atingupta-20201028-vm4.eastus.cloudapp.azure.com" "atingupta-20201028-vm5.eastus.cloudapp.azure.com" )
+ 
+all_hosts=$(join_by :5000, ${StringArray[@]})
+all_hosts="${all_hosts}:5000"
+echo $all_hosts
+
+i=1
+# Iterate the string array using for loop
+for host in ${StringArray[@]}; do
+   echo $host
+   hostfilename=vm$i.neo4j.conf.inc.txt
+   echo $hostfilename
+   rm -rf $hostfilename
+   cp neo4j.conf.template.txt $hostfilename
+   
+   sed -i "s/<hostname>/$host/g" $hostfilename
+   sed -i "s/<allhostnames>/$all_hosts/g" $hostfilename
+   
+   cat /var/lib/neo4j/conf/neo4j.conf $hostfilename > /var/lib/neo4j/conf/neo4j.conf
+   
+   ((i=i+1))
+done
+
+# cp neo4j.conf.template.txt neo4j.conf.inc.vm1.txt
+# sed -i 's/<hostname>/hostname_vm1/g' neo4j.conf.inc.vm1.txt
+# sed -i 's/<hostname1>/hostname1_vm1/g' neo4j.conf.inc.vm1.txt
+# sed -i 's/<hostname2>/hostname1_vm2/g' neo4j.conf.inc.vm1.txt
+# sed -i 's/<hostname3>/hostname1_vm3/g' neo4j.conf.inc.vm1.txt
+# sed -i 's/<hostname4>/hostname1_vm4/g' neo4j.conf.inc.vm1.txt
+# sed -i 's/<hostname5>/hostname1_vm5/g' neo4j.conf.inc.vm1.txt
